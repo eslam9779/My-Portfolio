07aa6850cf4cd07017d57f94b63ac2b0d54abfde
Eslam Abdelsatar <eslam.abdelsatar@almotahida.local>
2023-12-20 16:37:44 +0200

    update portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,8 +2,9 @@ import React, { useState } from 'react';
 import './portfolio.css';
 import tebian from '../../assets/tebian.png';
 import Gpt from '../../assets/Gpt.png';
-import youTube from '../../assets/youTube.png';
-
+import youTube from '../../assets/YouTube.jpg';
+import Twitter from '../../assets/Twitter.avif';
+import lifeMarket from '../../assets/lifeMarket.png'
 const Portfolio = () => {
 
     const [type, setType] = useState("");
@@ -12,86 +13,91 @@ const Portfolio = () => {
         {
             image: Gpt,
             header: "Chat Gpt-3",
-            text: "An educational platform for schools and teachers that aims to improve student performance and develop the teaching and learning process",
-            githup: "",
+            text: "Landing Page For Chat Gpt-3",
             type: "landing",
-            livePreview: "https://tebian.app/"
-        }, {
-            image: youTube,
-            header: "youTube Clone",
-            // text: "An educational platform for schools and teachers that aims to improve student performance and develop the teaching and learning process",
-            githup: "",
-            type: "social",
-
-            livePreview: "https://tebian.app/"
-        }, {
-            image: tebian,
-            header: "Tebian App",
-            // text: "An educational platform for schools and teachers that aims to improve student performance and develop the teaching and learning process",
-            githup: "",
-            type: "education",
-            livePreview: "https://tebian.app/"
+            link: "https://exquisite-marigold-dd26ed.netlify.app/"
         },
         {
-            image: Gpt,
-            header: "Chat Gpt-3",
-            text: "An educational platform for schools and teachers that aims to improve student performance and develop the teaching and learning process",
-            githup: "",
-            type: "landing",
-            livePreview: "https://tebian.app/"
-        }, {
-            image: youTube,
-            header: "youTube Clone",
-            // text: "An educational platform for schools and teachers that aims to improve student performance and develop the teaching and learning process",
-            githup: "",
-            type: "social",
-            livePreview: "https://tebian.app/"
-        }, {
             image: tebian,
             header: "Tebian App",
-            // text: "An educational platform for schools and teachers that aims to improve student performance and develop the teaching and learning process",
-            githup: "",
+             text: "An educational platform for schools and teachers that aims to improve student performance and develop the teaching and learning process",
             type: "education",
-            livePreview: "https://tebian.app/"
+            link: "https://tebian.app/"
+        }, 
+        {
+            image: youTube,
+            header: "youTube Clone",
+            text: "",
+            type: "social",
+            link: ""
+        }, 
+        {
+            image: Twitter,
+            header: "Twitter Clone",
+            text: "",
+            type: "landing",
+            link: ""
+        }, {
+            image: "",
+            header: "Coffe Shop System",
+            text: "",
+            type: "social",
+            link: ""
+        }, {
+            image: lifeMarket,
+            header: "Life Market E-Commerce UI",
+            text: "",
+            type: "ECommerce",
+            link: ""
+        },
+        {
+            image: lifeMarket,
+            header: "Life Market E-Commerce BackEnd ",
+            text: "",
+            type: "ECommerce",
+            link: "https://github.com/eslam9779/Life-market-Ecommerce-API"
         },
     ]
+    const goToProject =(item)=>{
+
+        window.location.href = item.link;
+
+    }
+
 
 
     return (
-        <div className='portfolio section__padding' id='Portfolio'>
+        <div className='portfolio section__padding'  id='Portfolio'>
             <div className="section-title">
                 <h2 className='gradient__text'>Projects</h2>
                 <p>Things I’ve built so far</p>
             </div>
 
-            <div className='portfolio__containeer-buttons'>
+            <div className='portfolio__buttons'>
 
                 <button className={type == "" ? 'active' : ''} onClick={() => setType("")}>All</button>
                 <button className={type == "social" ? 'active' : ''} onClick={() => setType("social")}>Social Media</button>
                 <button className={type == "landing" ? 'active' : ''} onClick={() => setType("landing")}>Landing Pages</button>
                 <button className={type == "education" ? 'active' : ''} onClick={() => setType("education")}>Education</button>
+                <button className={type == "ECommerce" ? 'active' : ''} onClick={() => setType("ECommerce")}>E-Commerce</button>
 
             </div>
 
-            <div className='portfolio__containeer'>
+            <div className='portfolio__containeer section__padding' >
 
                 {projects.filter((arrItem) => (type === "" ? true : arrItem.type === type)).map((item, index) =>
 
-                    <div className="portfolio__containeer-card" key={index}  style={{ backgroundImage: `url(${item.image})`,height:"250px",backgroundSize:"cover",backgroundRepeat:"no-repeat" }}>
-                        <img src={item.image} alt="Avatar" className="portfolio__containeer-card_image" />
-                        <div className="portfolio__containeer-card_middle">
-                            <div className="portfolio__containeer-card_text">
-                                <h1 className='header'>{item.header}</h1>
-                                <div className='portfolio__containeer-card_links'>
-                                    <a href={item.livePreview}> <p>Show Live Preview</p></a>
-                                    <a href={item.livePreview}> <p>GitHup</p></a>
-                                </div>
-                            </div>
+                    <div>
+                        <div className="portfolio__containeer-card " key={index} onClick={()=>goToProject(item)}>
+                            <img src={item.image} className="portfolio__containeer-card-image" />
+                            <div className="portfolio__containeer-card-text">{item.text}</div>
                         </div>
-                    </div>   
-                    
+                        <div className='Project_header'><h4>{item.header}</h4></div>
+
+                    </div>
+
                 )}
-                
+
             </div>
 
         </div>
